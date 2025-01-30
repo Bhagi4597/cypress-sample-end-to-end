775f21c5d5910af6b473e4ebde327eeb340da28b
Bhagyashri <bhagi4597@gmail.com>
2025-01-30 13:11:29 +0100

    Added 2 test cases for selecting radio button and verification

--- cypress/integration/Sample.js
+++ cypress/integration/Sample.js
@@ -1,5 +1,6 @@
-// Sample.js created with Cypress
+const { after } = require("node:test")
 
+// Sample.js created with Cypress
 describe('example to-do app', () => {
     beforeEach(() => {
     // visit the cypress.io website
@@ -13,5 +14,34 @@ describe('example to-do app', () => {
         // verify text on that 2 elemnts
         cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
         cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+      })
+
+      it('select pay electric bill item', () => {
+        // select pay electric bill
+        cy.get("li:nth-of-type(1) input").click();
+        // verify it selected
+        cy.get("footer > button").should('be.visible')
+        cy.get("footer > button").should('have.text','Clear completed')
+
+        // click on clear completed
+        cy.get("footer > button").click();
+        // verify it shows 1 item left & wont show clear completed button
+        cy.get("footer > button").should('not.be.visible')
+
+      })
+
+      it('select walk the dog', () => {
+        // select walk the dog
+        cy.get("li:nth-of-type(2) input").click();
+        // verify it selected
+        cy.get("footer > button").should('be.visible')
+        cy.get("footer > button").should('have.text','Clear completed')
+
+        // click on clear completed
+        cy.get("footer > button").click();
+        // verify it shows 1 item left & wont show clear completed button
+        cy.get("footer > button").should('not.be.visible')
+
+        
       })
 })
